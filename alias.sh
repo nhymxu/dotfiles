9ce666563bc37320d0251c9e2ac7fb81c71cc1b8
Dung Nguyen <contact@dungnt.net>
2018-11-14 16:48:06 +0700

    speedup zsh

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
-alias pip_upgrade="pip install -U `pip list --format=columns --outdated | awk '!/Package|---/{ print $1}'`"
+# alias pip_upgrade="pip install -U `pip list --format=columns --outdated | awk '!/Package|---/{ print $1}'`"
 alias prp="pipenv run python"
