ad63c88f2aa7fda2a0d93794becbf306c2554d73
Dung Nguyen <contact@dungnt.net>
2019-02-18 00:27:36 +0700

    weather alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,3 @@
 # alias pip_upgrade="pip install -U `pip list --format=columns --outdated | awk '!/Package|---/{ print $1}'`"
 alias prp="pipenv run python"
+weather() { curl -4 wttr.in/${1:-hanoi} }
