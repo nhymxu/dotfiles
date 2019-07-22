e6181eedbef9d71e4270666a8a5b9cbb947befe5
Dung Nguyen <contact@dungnt.net>
2019-07-22 11:30:06 +0700

    add exa alias

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,3 +1,6 @@
 # alias pip_upgrade="pip install -U `pip list --format=columns --outdated | awk '!/Package|---/{ print $1}'`"
 alias prp="pipenv run python"
 weather() { curl -4 wttr.in/${1:-hanoi} }
+
+# exa:
+alias la="exa -abghl --git"
