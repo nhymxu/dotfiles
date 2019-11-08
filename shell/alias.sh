31b24d3c18fedc92ac83940fd81e59be2c2c9d77
Dung Nguyen <contact@dungnt.net>
2019-11-08 21:34:40 +0700

    add some quick alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -4,3 +4,14 @@ weather() { curl -4 wttr.in/${1:-hanoi} }
 
 # exa:
 alias la="exa -abghl --git"
+
+# Show hidden files
+alias l.='ls -d .* --color=auto'
+
+# Start calculator with math support
+alias bc='bc -l'
+
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
