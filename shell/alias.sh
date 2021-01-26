32e5c04f10d0bcf700306c1c23f495f02dfba1ee
Dung Nguyen <contact@dungnt.net>
2021-01-26 22:18:38 +0700

    add some shell alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -16,6 +16,11 @@ alias now='date +"%T"'
 alias nowtime=now
 alias nowdate='date +"%d-%m-%Y"'
 
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
 function pip_upgrade() {
     pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
     # pip install -U `pip list --format=columns --outdated | awk '!/Package|---/{ print $1}'`
@@ -39,11 +44,11 @@ command_exists () {
 }
 
 rxvt-title() {
-  echo -n "]2;$*"
+    echo -n "]2;$*"
 }
 
 screen-title() {
-  echo -n "k$*\\"
+    echo -n "k$*\\"
 }
 
 renice_ggbackup() {
@@ -58,4 +63,3 @@ renice_ggbackup() {
 restore_ggbackup () {
     for f in $(pgrep 'Backup and Sync'; pgrep 'FinderSyncAPIExtension'); do renice 0 -p $f; done
 }
-
--- shell/macos.sh
+++ shell/macos.sh
@@ -8,3 +8,24 @@ function clear_dns() {
 }
 
 alias pyenvi='CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" pyenv install'
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+
+# Kill all the tabs in Chrome to free up memory
+# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# Reload the shell (i.e. invoke as a login shell)
+alias shell-reload="exec ${SHELL} -l"
+
+# Print each PATH entry on a separate line
+alias print-path='echo -e ${PATH//:/\\n}'
