6d19ebeb543e3b973f36557a34d2332d80c5d021
Dung Nguyen <contact@dungnt.net>
2021-01-27 16:34:21 +0700

    add zsh history option

--- shell/history.zsh
+++ shell/history.zsh
@@ -7,6 +7,14 @@ if [ -w ~/.zsh_history -o -w ~ ]; then
     # Append rather than overwrite the history on disk
     # shopt -s histappend
 
+    ## History command configuration
+    setopt extended_history       # record timestamp of command in HISTFILE
+    setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+    setopt hist_ignore_dups       # ignore duplicated commands history list
+    setopt hist_ignore_space      # ignore commands that start with space
+    setopt hist_verify            # show command with history expansion to user before running it
+    setopt share_history          # share command history data
+
     # Add timestamp to history file
     export HISTTIMEFORMAT="%d/%m/%y %T "
 
