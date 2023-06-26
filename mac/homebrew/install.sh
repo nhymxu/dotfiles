f3d33da4cb4b21419c71c16c49350826563d9ac1
Dung Nguyen <contact@dungnt.net>
2023-06-26 13:37:23 +0700

    tmp

---
+++ mac/homebrew/install.sh
@@ -0,0 +1,5 @@
+cd ~/dotfiles/mac/homebrew
+brew bundle --file ~/dotfiles/mac/homebrew/fomula/core.txt
+brew bundle --file ~/dotfiles/mac/homebrew/fomula/brews.txt
+brew bundle --file ~/dotfiles/mac/homebrew/fomula/casks.txt
+brew bundle --file ~/dotfiles/mac/homebrew/fomula/extras.txt
---
+++ mac/homebrew/test.sh
@@ -0,0 +1,27 @@
+# CLI
+brew install exa
+brew install bat
+brew install dust
+brew install duf
+brew install fd
+brew install dog
+brew install glances
+
+brew install docker docker-compose
+brew install tig
+brew install wget
+brew install nvm
+
+# Casks / Apps
+brew install --cask iterm2
+brew install --cask zsh
+brew install --cask brave
+
+brew install --cask monitorcontrol
+brew install --cask rectangle
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask virtualbox virtualbox-extension-pack
+brew install --cask libreoffice
+
+# https://github.com/serhii-londar/open-source-mac-os-apps
