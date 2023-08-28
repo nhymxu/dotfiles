f8030de98063c586d494b45f6dc44137a2605c20
Dung Nguyen <contact@dungnt.net>
2023-08-28 22:15:54 +0700

    asdf php & caddy

--- mac/brew-setup.sh
+++ mac/brew-setup.sh
@@ -80,6 +80,8 @@ asdf plugin-add php
 brew install libpq pkg-config bison bzip2 freetype gettext libiconv icu4c jpeg libedit libpng libxml2 libzip openssl@1.1 readline webp zlib re2c gd krb5 oniguruma   # require to build php
 
 asdf plugin-add nodejs
+asdf plugin-add php
+asdf plugin-add caddy
 
 brew install mysql-client
 
