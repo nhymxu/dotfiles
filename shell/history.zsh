2277732babe3d4c6f7f5b8713bce1e9b98843448
nhymxu <contact@dungnt.net>
2024-04-16 17:52:15 +0700

    shell: zsh history config

--- shell/history.zsh
+++ shell/history.zsh
@@ -8,18 +8,23 @@
     # shopt -s histappend
 
     ## History command configuration
-    setopt extended_history       # record timestamp of command in HISTFILE
+    setopt EXTENDED_HISTORY       # record timestamp of command in HISTFILE
+    setopt INC_APPEND_HISTORY    # Write to the history file immediately, not when the shell exits.
+    setopt SHARE_HISTORY          # share command history data between all sessions
+    setopt HIST_IGNORE_DUPS      # Do not record an event that was just recorded again.
+    setopt HIST_IGNORE_ALL_DUPS  # Delete an old recorded event if a new event is a duplicate.
+    setopt HIST_IGNORE_SPACE      # ignore commands that start with space
+    setopt HIST_SAVE_NO_DUPS     # Do not write a duplicate event to the history file.
+    setopt HIST_VERIFY           # Do not execute immediately upon history expansion. show command with history expansion to user before running it
+    setopt APPEND_HISTORY        # append to history file (Default)
     setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-    setopt hist_ignore_dups       # ignore duplicated commands history list
-    setopt hist_ignore_space      # ignore commands that start with space
-    setopt hist_verify            # show command with history expansion to user before running it
-    setopt share_history          # share command history data
     setopt hist_find_no_dups       # ignore duplicated commands history when find
-    setopt INC_APPEND_HISTORY     # Immediate append
-    setopt HIST_IGNORE_ALL_DUPS
+    setopt HIST_NO_STORE          # Don't store history commands
+    setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
 
     # Add timestamp to history file
     export HISTTIMEFORMAT="%d/%m/%y %T "
+    # HIST_STAMPS="yyyy-mm-dd"
 
     # Don't put duplicate lines in the history.
     # export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoreboth:erasedups
@@ -32,8 +37,9 @@
     # HISTIGNORE is a colon-delimited list of patterns which should be excluded.
     #         The '&' is a special pattern which suppresses duplicate entries.
     export HISTIGNORE=$'[ \t]*:&:[fb]g:pwd:exit:fg:bg:top:ls:uptime:df:clear:composer:pip:poetry:git:history*'
+    # HISTORY_IGNORE="(ls|pwd|cd)*"
 
     # Export history to a different file
     # export HISTFILE="${HOME}/.history/${CURRENT_SHELL}.history.txt"
-    export HISTFILE=~/.zsh_history
+    export HISTFILE="$HOME/.zsh_history"
 #fi
